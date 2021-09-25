2ae759e4d6d43494741b5d9c93cc4872745b210f
icancodebeauty <admin@itsjava.ml>
2021-09-25 16:12:51 +0000

    Till Lesson 1

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main() {
-   printf("Hello, This is Aditya!!");
+   int num1, num2;
+   printf("Enter First Number\n");
+   scanf("%d", &num1);
+   printf("Enter Second Number\n");
+   scanf("%d", &num2);
+   printf("The Sum Of Given Two Numbers is %d\n", num1 + num2);
    return 0;
 }
