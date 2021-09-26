e064449fde3dd2aa715b59b21f86ac4d1e592995
icancodebeauty <admin@itsjava.ml>
2021-09-26 16:22:34 +0000

    Completed Chapter 1

--- Chapter 1 Exercise/C2F.c
+++ Chapter 1 Exercise/C2F.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
     const float ninebyfive = 1.8; // Value of 9/5
---
+++ Chapter 1 Exercise/MultiplicationTable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Enter Number :- ");
+    scanf("%d", &num);
+    printf("%d × 1 = %d\n", num, num * 1);
+    printf("%d × 2 = %d\n", num, num * 2);
+    printf("%d × 3 = %d\n", num, num * 3);
+    printf("%d × 4 = %d\n", num, num * 4);
+    printf("%d × 5 = %d\n", num, num * 5);
+    printf("%d × 6 = %d\n", num, num * 6);
+    printf("%d × 7 = %d\n", num, num * 7);
+    printf("%d × 8 = %d\n", num, num * 8);
+    printf("%d × 9 = %d\n", num, num * 9);
+    printf("%d × 10 = %d\n", num, num * 10);
+    return 0;
+}
+// I made this program without using loop because it was not studied!
